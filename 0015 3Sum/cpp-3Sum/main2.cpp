0579807703526b0626429ed50b9a14749bde94bd
liuyubobobo <liuyubobobo@gmail.com>
2016-12-09 09:17:35 +0800

    0015 code updated.

--- 0015 3Sum/cpp-3Sum/main2.cpp
+++ 0015 3Sum/cpp-3Sum/main2.cpp
@@ -12,9 +12,10 @@ using namespace std;
 
 
 /// Problem
-/*****************************************************************************************************
+/********************************************************************************************
  *
- * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+ * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
  *
  * Note: The solution set must not contain duplicate triplets.
  *
@@ -25,7 +26,7 @@ using namespace std;
  *  [-1, 0, 1],
  *  [-1, -1, 2]
  * ]
- *****************************************************************************************************/
+ *********************************************************************************************/
 
 
 /// Solution 2
