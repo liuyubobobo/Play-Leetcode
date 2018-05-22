74e1aa8c9af326561b8b96b1a90d65465b495f09
liuyubobobo <liuyubobobo@gmail.com>
2018-05-22 09:41:45 +0800

    0098 java solution added.

---
+++ 0098-Validate-Binary-Search-Tree/java-0098/src/Solution.java
@@ -0,0 +1,30 @@
+/// Source : https://leetcode.com/problems/validate-binary-search-tree/description/
+/// Author : liuyubobobo
+/// Time   : 2018-05-22
+
+import java.util.ArrayList;
+
+/// Using inOrder traverse
+/// Time Complexity: O(n)
+/// Space Complexity: O(n)
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        for(int i = 1 ; i < list.size() ; i ++)
+            if(list.get(i - 1) >= list.get(i))
+                return false;
+        return true;
+    }
+
+    private void inOrder(TreeNode node, ArrayList<Integer> list){
+
+        if(node == null)
+            return;
+
+        inOrder(node.left, list);
+        list.add(node.val);
+        inOrder(node.right, list);
+    }
+}
---
+++ 0098-Validate-Binary-Search-Tree/java-0098/src/Solution2.java
@@ -0,0 +1,39 @@
+/// Source : https://leetcode.com/problems/validate-binary-search-tree/description/
+/// Author : liuyubobobo
+/// Time   : 2018-05-22
+
+/// Recursive test
+/// Time Complexity: O(n)
+/// Space Complexity: O(h)
+class Solution2 {
+    public boolean isValidBST(TreeNode root) {
+
+        return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode node, int min, int max){
+
+        if(node == null)
+            return true;
+
+        if(node.val < min || node.val > max)
+            return false;
+
+        if(node.left != null && node.left.val >= node.val)
+            return false;
+
+        if(node.right != null && node.right.val <= node.val)
+            return false;
+
+        return isValidBST(node.left, min, node.val - 1) &&
+                isValidBST(node.right, node.val + 1, max);
+    }
+
+    public static void main(String[] args){
+
+        TreeNode root = new TreeNode(-2147483648);
+        root.left = new TreeNode(-2147483648);
+
+        System.out.println((new Solution2()).isValidBST(root));
+    }
+}
