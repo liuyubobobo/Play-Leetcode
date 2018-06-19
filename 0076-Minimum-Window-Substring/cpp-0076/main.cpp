b78a9e79ec04e70affec7f71becb0976756dc7c3
liuyubobobo <liuyubobobo@gmail.com>
2018-06-19 01:08:56 -0700

    0076 solved.

---
+++ 0076-Minimum-Window-Substring/cpp-0076/main.cpp
@@ -0,0 +1,66 @@
+/// Source : https://leetcode.com/problems/minimum-window-substring/
+/// Author : liuyubobobo
+/// Time   : 2017-01-17
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+/// Sliding Window
+/// Time Complexity: O(n)
+/// Space Complexity: O(1)
+class Solution {
+public:
+    string minWindow(string s, string t) {
+
+        int tFreq[256] = {0};
+        for(int i = 0 ; i < t.size() ; i ++)
+            tFreq[t[i]] ++;
+
+        int sFreq[256] = {0};
+        int sCnt = 0;
+
+        int minLength = s.size() + 1;
+        int startIndex = -1;
+
+        int l = 0, r = -1;
+        while(l < s.size()){
+
+            if(r + 1 < s.size() && sCnt < t.size()){
+
+                sFreq[s[r+1]] ++;
+                if(sFreq[s[r+1]] <= tFreq[s[r+1]])
+                    sCnt ++;
+                r ++;
+            }
+            else{
+                assert(sCnt <= t.size());
+                if(sCnt == t.size() && r - l + 1 < minLength){
+                    minLength = r - l + 1;
+                    startIndex = l;
+                }
+
+                sFreq[s[l]] --;
+                if(sFreq[s[l]] < tFreq[s[l]])
+                    sCnt --;
+                l ++;
+            }
+        }
+
+        if( startIndex != -1 )
+            return s.substr(startIndex, minLength);
+
+        return "";
+    }
+};
+
+int main() {
+
+    cout << Solution().minWindow("ADOBECODEBANC", "ABC") << endl;
+    cout << Solution().minWindow("a", "aa") << endl;
+    cout << Solution().minWindow("aa", "aa") << endl;
+    cout << Solution().minWindow("bba", "ab") << endl;
+
+    return 0;
+}
