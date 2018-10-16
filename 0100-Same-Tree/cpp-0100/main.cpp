9a40f58b6634520a8375a1f276e4ebeae4ba164f
liuyubobobo <liuyubobobo@gmail.com>
2018-10-16 14:43:27 -0700

    0100 solved.

---
+++ 0100-Same-Tree/cpp-0100/main.cpp
@@ -0,0 +1,37 @@
+/// Source : https://leetcode.com/problems/same-tree/description/
+/// Author : liuyubobobo
+/// Time   : 2018-10-16
+
+#include <iostream>
+
+using namespace std;
+
+
+/// Recursion
+/// Time Complexity: O(n)
+/// Space Complexity: O(h)
+
+/// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        if(p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+
+int main() {
+
+    return 0;
+}
