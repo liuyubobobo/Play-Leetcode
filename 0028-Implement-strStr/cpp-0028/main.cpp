47ad32ff6513fc95ac5553c6105ede2dd878fe68
Yubo Liu <liuyubobobo@gmail.com>
2019-03-12 11:02:31 -0700

    0028 new algo added.

--- 0028-Implement-strStr/cpp-0028/main.cpp
+++ 0028-Implement-strStr/cpp-0028/main.cpp
@@ -1,15 +1,21 @@
+/// Source : https://leetcode.com/problems/implement-strstr/
+/// Author : liuyubobobo
+/// Time   : 2018-03-06
+
 #include <iostream>
 
 using namespace std;
 
+
+/// Naive implementation
+/// Time Complexity: O(n * m)
+/// Sapce Complexity: O(1)
 class Solution {
 public:
     int strStr(string haystack, string needle) {
 
-        if(needle.size() > haystack.size())
-            return -1;
-
-        for(int i = 0 ; i <= haystack.size() - needle.size() ; i ++){
+        int n = haystack.size(), m = needle.size();
+        for(int i = 0 ; i <= n - m ; i ++){
             int j = 0;
             for(j = 0 ; j < needle.size() ; j ++)
                 if(needle[j] != haystack[j + i])
---
+++ 0028-Implement-strStr/cpp-0028/main2.cpp
@@ -0,0 +1,72 @@
+/// Source : https://leetcode.com/problems/implement-strstr/
+/// Author : liuyubobobo
+/// Time   : 2019-03-11
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+/// KMP based on lps
+/// Time Complexity: O(n + m)
+/// Sapce Complexity: O(m)
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        if(haystack == "") return needle == "" ? 0 : -1;
+        if(needle == "") return 0;
+
+        int n = haystack.size(), m = needle.size();
+        vector<int> lps = getLPS(needle, m); // longest proper prefix which is also suffix
+
+        int i = 0, j = 0;
+        while(i < n){
+
+            if(haystack[i] == needle[j]) {
+                i++, j++;
+
+                if (j == needle.size())
+                    return i - needle.size();
+            }
+
+            else if(j) j = lps[j - 1] ;
+            else i ++;
+        }
+        return -1;
+    }
+
+private:
+    vector<int> getLPS(const string& pattern, int m){
+
+        vector<int> lps(m, 0);
+        // lps[0] = 0;
+
+        int len = 0;
+        int i = 1;
+        while(i < m)
+            if(pattern[i] == pattern[len])
+                len ++, lps[i] = len, i ++;
+            else{
+                // trick part
+                // a great explanation:
+                // https://leetcode.com/problems/implement-strstr/discuss/13160/detailed-explanation-on-building-up-lps-for-kmp-algorithm
+                if(len) len = lps[len - 1];
+                else
+                    lps[i] = 0,
+                    i ++;
+            }
+        return lps;
+    }
+};
+
+
+int main() {
+
+    cout << Solution().strStr("hello", "ll") << endl; // 2
+    cout << Solution().strStr("aaaaa", "bba") << endl; // -1
+    cout << Solution().strStr("mississippi", "issip") << endl; // 4
+
+    return 0;
+}
