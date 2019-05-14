2c4ec28ef3e75e6cc907d0c6ca20ba7065466e6c
Yubo Liu <liuyubobobo@gmail.com>
2019-05-14 00:56:21 -0700

    0937 bug fixed.

--- 0937-Reorder-Log-File/cpp-0937/main2.cpp
+++ 0937-Reorder-Log-File/cpp-0937/main2.cpp
@@ -16,7 +16,7 @@ class Solution {
 public:
     vector<string> reorderLogFiles(vector<string>& logs) {
 
-        sort(logs.begin(), logs.end(), [](const string& log1, const string& log2) -> bool{
+        stable_sort(logs.begin(), logs.end(), [](const string& log1, const string& log2) -> bool{
             int space1 = log1.find(' ');
             string id1 = log1.substr(0, space1), after1 = log1.substr(space1 + 1);
 
