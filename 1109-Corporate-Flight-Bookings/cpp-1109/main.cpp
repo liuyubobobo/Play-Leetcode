b8ffa270bb4a978242f8d0b5fbe8be5835e597ba
Yubo Liu <liuyubobobo@gmail.com>
2019-07-07 10:39:41 -0700

    1109 added.

---
+++ 1109-Corporate-Flight-Bookings/cpp-1109/main.cpp
@@ -0,0 +1,36 @@
+/// Source : https://leetcode.com/problems/corporate-flight-bookings/
+/// Author : liuyubobobo
+/// Time   : 2019-07-07
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+/// Dealing with Interview
+/// Start and Ending events
+/// Time Complexity: O(|bookings| + n)
+/// Space Complexity: O(1)
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+
+        vector<int> res(n + 1, 0);
+        for(const vector<int>& booking: bookings){
+            res[booking[0] - 1] += booking[2];
+            res[booking[1]] -= booking[2];
+        }
+
+        for(int i = 1; i <= n; i ++)
+            res[i] += res[i - 1];
+        res.pop_back();
+        return res;
+    }
+};
+
+
+int main() {
+
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -751,4 +751,5 @@ email: [liuyubobobo@gmail.com](mailto:liuyubobobo@gmail.com)
 | 1099 | [Two Sum Less Than K](https://leetcode.com/problems/two-sum-less-than-k/) | [无] | [C++](1099-Two-Sum-Less-Than-K/cpp-1099/) | | |
 | | | | | | |
 | 1108 | [Defanging an IP Address](https://leetcode.com/problems/defanging-an-ip-address/) | [无] | [C++](1108-Defanging-an-IP-Address/cpp-1108/) | | |
+| 1109 | [Corporate Flight Bookings](https://leetcode.com/problems/corporate-flight-bookings/) | [无] | [C++](1109-Corporate-Flight-Bookings/cpp-1009/) | | |
 | | | | | | |
