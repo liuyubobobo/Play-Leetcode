c5de38de2b6aee528b9fc05fef33d45d578e4b07
Yubo Liu <liuyubobobo@gmail.com>
2019-09-01 23:35:36 -0700

    211 java codes updated.

--- 0211-Add-and-Search-Word-Data-structure-design/java-0211/src/WordDictionary.java
+++ 0211-Add-and-Search-Word-Data-structure-design/java-0211/src/WordDictionary.java
@@ -1,65 +1,69 @@
 /// Source : https://leetcode.com/problems/add-and-search-word-data-structure-design/
 /// Author : liuyubobobo
-/// Time   : 2019-04-07
-
-import java.util.HashMap;
-
+/// Time   : 2019-09-01
 
 /// Trie
 /// Time Complexity: addWord: O(len(word))
 ///                  search:  O(size(trie))
 /// Space Complexity: O(sum(len(wi))) where wi is the length of the ith word
-class WordDictionary {
+public class WordDictionary {
 
     private class Node{
 
-        public HashMap<Character, Node> next = new HashMap<>();
-        public boolean end = false;
+        public boolean isWord;
+        public Node next[];
+
+        public Node(boolean isWord){
+            this.isWord = isWord;
+            next = new Node[26];
+        }
+
+        public Node(){
+            this(false);
+        }
     }
 
-    private Node trie;
+    private Node root;
 
     /** Initialize your data structure here. */
     public WordDictionary() {
-        trie = new Node();
+        root = new Node();
     }
 
     /** Adds a word into the data structure. */
     public void addWord(String word) {
 
-        Node cur = trie;
-        for(int i = 0; i < word.length(); i ++){
+        Node cur = root;
+        for(int i = 0 ; i < word.length() ; i ++){
             char c = word.charAt(i);
-            if(cur.next.get(c) == null)
-                cur.next.put(c, new Node());
-            cur = cur.next.get(c);
+            if(cur.next[c - 'a'] == null)
+                cur.next[c - 'a'] = new Node();
+            cur = cur.next[c - 'a'];
         }
-
-        cur.end = true;
+        cur.isWord = true;
     }
 
     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
     public boolean search(String word) {
-        return search(trie, word, 0);
+        return match(root, word, 0);
     }
 
-    private boolean search(Node node, String word, int index){
+    private boolean match(Node node, String word, int index){
 
         if(index == word.length())
-            return node.end;
+            return node.isWord;
 
         char c = word.charAt(index);
-        if(c != '.'){
-            if(node.next.get(c) == null)
-                return false;
 
-            return search(node.next.get(c), word, index + 1);
+        if(c != '.'){
+            if(node.next[c - 'a'] == null)
+                return false;
+            return match(node.next[c - 'a'], word, index + 1);
         }
         else{
-            for(char key: node.next.keySet())
-                if(search(node.next.get(key), word, index + 1))
+            for(char nextChar = 'a'; nextChar <= 'z'; nextChar ++)
+                if(node.next[nextChar - 'a'] != null && match(node.next[nextChar - 'a'], word, index + 1))
                     return true;
-
             return false;
         }
     }
