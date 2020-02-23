66c1211dbc0abb20636c5cfa95ccfbd51e321641
Yubo Liu <liuyubobobo@gmail.com>
2020-02-22 21:48:07 -0800

    1362 added.

--- 1362-Closest-Divisors/cpp-1362/main.cpp
+++ 1362-Closest-Divisors/cpp-1362/main.cpp
@@ -1,17 +1,22 @@
+/// Source : https://leetcode.com/problems/closest-divisors/
+/// Author : liuyubobobo
+/// Time   : 2020-02-22
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 
+/// Brute Force
+/// Time Complexity: O(sqrt(num))
+/// Space Complexity: O(1)
 class Solution {
 public:
     vector<int> closestDivisors(int num) {
 
         vector<int> v1= getDivisors(num + 1);
-//        Solution().print_vec(v1);
         vector<int> v2 = getDivisors(num + 2);
-//        Solution().print_vec(v2);
 
         return v1[1] - v1[0] < v2[1] - v2[0] ? v1 : v2;
     }
@@ -25,28 +30,25 @@ private:
                 res = {i, x / i};
         return res;
     }
-
-public:
-    static void print_vec(const vector<int>& vec){
-        for(int e: vec) cout << e << " "; cout << endl;
-    }
 };
 
 
-
+void print_vec(const vector<int>& vec){
+    for(int e: vec) cout << e << " "; cout << endl;
+}
 
 int main() {
 
-    Solution().print_vec(Solution().closestDivisors(8));
+    print_vec(Solution().closestDivisors(8));
     // 3 3
 
-    Solution().print_vec(Solution().closestDivisors(123));
+    print_vec(Solution().closestDivisors(123));
     // 5 25
 
-    Solution().print_vec(Solution().closestDivisors(999));
+    print_vec(Solution().closestDivisors(999));
     // 40 25
 
-    Solution().print_vec(Solution().closestDivisors(170967091));
+    print_vec(Solution().closestDivisors(170967091));
     // 10754 15898
 
     return 0;
