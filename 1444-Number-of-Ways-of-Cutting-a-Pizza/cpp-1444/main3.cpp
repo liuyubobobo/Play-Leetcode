ea9340016b9a577448d61d1eb07611bf56d87a31
Yubo Liu <liuyubobobo@gmail.com>
2020-05-10 15:51:50 -0700

    1444 optimized algo added.

---
+++ 1444-Number-of-Ways-of-Cutting-a-Pizza/cpp-1444/main3.cpp
@@ -0,0 +1,74 @@
+/// Source : https://leetcode.com/problems/number-of-ways-of-cutting-a-pizza/
+/// Author : liuyubobobo
+/// Time   : 2020-05-10
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+/// Dynamic Programming with Space Optimization
+/// Time Complexity: O(k * R * C * (R + C))
+/// Space Complexity: O(R * C)
+class Solution {
+
+private:
+    const int MOD = 1e9 + 7;
+    int R, C;
+
+public:
+    int ways(vector<string>& pizza, int k) {
+
+        R = pizza.size(), C = pizza[0].size();
+        vector<vector<int>> cnt(R + 1, vector<int>(C + 1, 0));
+        for(int i = 0; i < R; i ++)
+            for(int j = 0; j < C; j ++)
+                cnt[i + 1][j + 1] = cnt[i][j + 1] + cnt[i + 1][j] - cnt[i][j] + (pizza[i][j] == 'A');
+
+        vector<vector<int>> dp(2, vector<int>(R * C, 0));
+        for(int i = 0; i < R; i ++)
+            for(int j = 0; j < C; j ++)
+                dp[0][i * C + j] = ok(cnt, i, j, R - 1, C - 1);
+
+        for(int t = 1; t < k; t ++)
+            for(int r = 0; r < R; r ++)
+                for(int c = 0; c < C; c ++){
+
+                    dp[t % 2][r * C + c] = 0;
+                    for(int i = r + 1; i < R; i ++)
+                        if(ok(cnt, r, c, i - 1, C - 1))
+                            dp[t % 2][r * C + c] += dp[(t - 1) % 2][i * C + c],
+                            dp[t % 2][r * C + c] %= MOD;
+
+                    for(int j = c + 1; j < C; j ++)
+                        if(ok(cnt, r, c, R - 1, j - 1))
+                            dp[t % 2][r * C + c] += dp[(t - 1) % 2][r * C + j],
+                            dp[t % 2][r * C + c] %= MOD;
+                }
+        return dp[(k - 1) % 2][0];
+    }
+
+private:
+    bool ok(const vector<vector<int>>& cnt, int x1, int y1, int x2, int y2){
+        return cnt[x2 + 1][y2 + 1] - cnt[x2 + 1][y1] - cnt[x1][y2 + 1] + cnt[x1][y1];
+    }
+};
+
+
+int main() {
+
+    vector<string> pizza1 = {"A..","AAA","..."};
+    cout << Solution().ways(pizza1, 3) << endl;
+    // 3
+
+    vector<string> pizza2 = {"A..","AA.","..."};
+    cout << Solution().ways(pizza2, 3) << endl;
+    // 1
+
+    vector<string> pizza3 = {"A..","A..","..."};
+    cout << Solution().ways(pizza3, 1) << endl;
+    // 1
+
+    return 0;
+}
