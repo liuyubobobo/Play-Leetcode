8a6b689d44b1c4d5485448441322c647e74a6053
Yubo Liu <liuyubobobo@gmail.com>
2020-05-16 13:36:03 -0700

    1447 new algo added.

--- 1447-Simplified-Fractions/cpp-1447/main2.cpp
+++ 1447-Simplified-Fractions/cpp-1447/main2.cpp
@@ -9,20 +9,21 @@
 using namespace std;
 
 
-/// Brute Force + GCD + HashSet
+/// Brute Force + GCD
 /// Time Complexity: O(n^2)
 /// Space Complexity: O(n^2)
 class Solution {
 public:
     vector<string> simplifiedFractions(int n) {
 
-        unordered_set<string> res;
+        vector<string> res;
         for(int d = 2; d <= n; d ++)
             for(int i = 1; i < d; i ++){
                 int g = gcd(d, i);
-                res.insert(to_string(i / g) + "/" + to_string(d / g));
+                if(g == 1)
+                    res.push_back(to_string(i) + "/" + to_string(d));
             }
-        return vector<string>(res.begin(), res.end());
+        return res;
     }
 
 private:
