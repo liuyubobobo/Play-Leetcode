1434792efaedadba5246f627bd1a08109ef0cfc2
Yubo Liu <liuyubobobo@gmail.com>
2020-05-25 19:29:44 -0700

    309 updated.

--- 0309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/cpp-0309/main4.cpp
+++ 0309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/cpp-0309/main4.cpp
@@ -25,7 +25,7 @@ public:
         dp[0][0] = -prices[0];
 
         for(int i = 1 ; i < n ; i ++){
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+            dp[i][0] = max(max(dp[i - 1][0], dp[i - 1][2] - prices[i]), dp[i - 1][1] - prices[i]);
             dp[i][1] = dp[i - 1][0] + prices[i];
             dp[i][2] = max(dp[i - 1][1], dp[i - 1][2]);
         }
