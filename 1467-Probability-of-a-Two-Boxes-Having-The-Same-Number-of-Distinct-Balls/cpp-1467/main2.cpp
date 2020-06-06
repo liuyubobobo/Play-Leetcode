9dc6ca8197bf36e6a874271a343f5095f9af6076
Yubo Liu <liuyubobobo@gmail.com>
2020-06-05 19:47:54 -0700

    1467 new algo added.

--- 1467-Probability-of-a-Two-Boxes-Having-The-Same-Number-of-Distinct-Balls/cpp-1467/main2.cpp
+++ 1467-Probability-of-a-Two-Boxes-Having-The-Same-Number-of-Distinct-Balls/cpp-1467/main2.cpp
@@ -30,24 +30,29 @@ public:
         for(int e: balls) all /= perm[e];
 //        cout << "all : " << all << endl;
 
-        double valid = dfs(balls, 0, 0, 0, n / 2, n / 2);
+        vector<double> dp(25 * 10 * 10 * 10, -1);
+        double valid = dfs(balls, 0, 0, 0, n / 2, n / 2, dp);
 //        cout << "valid : " << valid << endl;
 
         return valid / all;
     }
 
 private:
-    double dfs(vector<int>& balls, int index, int c1, int c2, int cnt1, int cnt2){
+    double dfs(vector<int>& balls, int index, int c1, int c2, int cnt1, int cnt2,
+               vector<double>& dp){
 
         if(index == balls.size())
             return cnt1 == 0 && cnt2 == 0 && c1 == c2;
 
+        int hashcode = cnt1 * 1000 + c2 * 100 + c1 * 10 + index;
+        if(dp[hashcode] != -1) return dp[hashcode];
+
         double res = 0.0;
         for(int i = 0; i <= balls[index] && i <= cnt1; i ++)
             if(balls[index] - i <= cnt2)
-                res += dfs(balls, index + 1, i ? c1 + 1 : c1, balls[index] - i ? c2 + 1 : c2, cnt1 - i, cnt2 - (balls[index] - i))
+                res += dfs(balls, index + 1, i ? c1 + 1 : c1, balls[index] - i ? c2 + 1 : c2, cnt1 - i, cnt2 - (balls[index] - i), dp)
                        * C(cnt1, i) * C(cnt2, balls[index] - i);
-        return res;
+        return dp[hashcode] = res;
     }
 
     double C(int a, int k){
