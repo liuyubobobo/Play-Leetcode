d73614471930c9aaef052d594cb463dfe7c9f35c
Yubo Liu <liuyubobobo@gmail.com>
2020-10-14 16:20:28 -0700

    0033 new algo added.

--- 0033-Search-in-Rotated-Sorted-Array/cpp-0033/main2.cpp
+++ 0033-Search-in-Rotated-Sorted-Array/cpp-0033/main2.cpp
@@ -1,43 +1,36 @@
 /// Source : https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 /// Author : liuyubobobo
-/// Time   : 2018-06-22
+/// Time   : 2020-10-14
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-/// Binary Search
-/// Time Complexity: O(logn)
+/// Find max value and do Binary Search
+/// Time Complexity: O(n)
 /// Space Complexity: O(1)
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
 
-        int l = 0, r = nums.size() - 1;
-        while(l <= r){
-            //cout << "l : " << l << " r : " << r << endl;
-            int mid = l + (r - l) / 2;
-            if(nums[mid] == target)
-                return mid;
-            //cout << "mid : " << mid << endl;
+        if(nums.size() == 0) return -1;
+        int max_index = max_element(nums.begin(), nums.end()) - nums.begin();
 
-            if(target < nums[mid]){
-                if(nums[l] <= nums[r] || nums[mid] < nums[l])
-                    r = mid - 1;
-                else if(target >= nums[l])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
-            else{ // target > nums[mid]
-                if(nums[l] <= nums[r] || nums[mid] > nums[r])
-                    l = mid + 1;
-                else if(target >= nums[l])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
+        int l = 0, r = max_index;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] == target) return mid;
+            if(target < nums[mid]) r = mid - 1;
+            else l = mid + 1;
+        }
+
+        l = max_index + 1, r = nums.size() - 1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] == target) return mid;
+            if(target < nums[mid]) r = mid - 1;
+            else l = mid + 1;
         }
 
         return -1;
