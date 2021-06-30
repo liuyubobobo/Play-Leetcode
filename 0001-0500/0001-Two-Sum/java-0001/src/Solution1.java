57db4a7efbd67c0ef61594b1ea852f30d3fdcaa7
xiaop1ng <jianchaoping@gmail.com>
2021-06-30 16:16:05 +0800

    Update Solution1.java
    例如:
    int[] nums = {3, 4, 2, 0};
    int target = 6;
    
    输出: [0, 0]
    
    正确输出: [1, 2]

--- 0001-0500/0001-Two-Sum/java-0001/src/Solution1.java
+++ 0001-0500/0001-Two-Sum/java-0001/src/Solution1.java
@@ -13,7 +13,7 @@ public class Solution1 {
 
         for(int i = 0 ; i < nums.length; i ++)
             for(int j = 0 ; j < nums.length ; j ++)
-                if(nums[i] + nums[j] == target){
+                if(i!=j && nums[i] + nums[j] == target){
                     int[] res = {i, j};
                     return res;
                 }
