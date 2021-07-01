e31470701c06bce85fd39a2056b72483a7065d9e
xiaop1ng <jianchaoping@gmail.com>
2021-07-01 13:24:47 +0800

    Update Solution1.java

--- 0001-0500/0001-Two-Sum/java-0001/src/Solution1.java
+++ 0001-0500/0001-Two-Sum/java-0001/src/Solution1.java
@@ -12,8 +12,8 @@ public class Solution1 {
     public int[] twoSum(int[] nums, int target) {
 
         for(int i = 0 ; i < nums.length; i ++)
-            for(int j = 0 ; j < nums.length ; j ++)
-                if(i!=j && nums[i] + nums[j] == target){
+            for(int j = i + 1 ; j < nums.length ; j ++)
+                if(nums[i] + nums[j] == target){
                     int[] res = {i, j};
                     return res;
                 }
