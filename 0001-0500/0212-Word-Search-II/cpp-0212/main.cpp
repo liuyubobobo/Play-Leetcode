dec02656d4bda1e9dd8458d51961f0e4dab1c99c
Yubo Liu <liuyubobobo@gmail.com>
2021-09-15 12:08:33 -0700

    212 codes updated.

--- 0001-0500/0212-Word-Search-II/cpp-0212/main.cpp
+++ 0001-0500/0212-Word-Search-II/cpp-0212/main.cpp
@@ -1,104 +1,53 @@
 /// Source : https://leetcode.com/problems/word-search-ii/
 /// Author : liuyubobobo
 /// Time   : 2019-02-08
+/// Updated: 2021-09-15
 
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <unordered_set>
+#include <set>
 
 using namespace std;
 
 
 /// Trie + DFS
-/// The Trie class is implemented in 208
-///
 /// Time Complexity: O(4 ^ (m * n) * maxlen)
 /// Space Complexity: O(m * n + total_letters_in_words)
-class Trie{
-
-private:
-    struct Node{
-        unordered_map<char, int> next;
-        bool end = false;
-    };
-    vector<Node> trie;
-
-public:
-    Trie(){
-        trie.clear();
-        trie.push_back(Node());
-    }
-
-    /** Inserts a word into the trie. */
-    void insert(const string& word){
-
-        int treeID = 0;
-        for(char c: word){
-            if(trie[treeID].next.find(c) == trie[treeID].next.end()){
-                trie[treeID].next[c] = trie.size();
-                trie.push_back(Node());
-            }
-
-            treeID = trie[treeID].next[c];
-        }
-
-        trie[treeID].end = true;
-    }
-
-    /** Returns if the word is in the trie. */
-    bool search(const string& word){
-
-        int treeID = 0;
-        for(char c: word){
-            if(trie[treeID].next.find(c) == trie[treeID].next.end())
-                return false;
-
-            treeID = trie[treeID].next[c];
-        }
-        return trie[treeID].end;
-    }
-
-    /** Returns if there is any word in the trie that starts with the given prefix. */
-    bool startsWith(const string& prefix) {
-
-        int treeID = 0;
-        for(char c: prefix){
-            if(trie[treeID].next.find(c) == trie[treeID].next.end())
-                return false;
-
-            treeID = trie[treeID].next[c];
-        }
-        return true;
-    }
-};
-
-
 class Solution {
 
 private:
-    int d[4][2] = {{-1, 0}, {0,1}, {1, 0}, {0, -1}};
-    int m, n;
-    int maxlen = 0;
+    class Node{
+    public:
+        vector<Node*> next;
+        bool end = false;
+
+        Node() : next(26, nullptr){};
+    };
+    Node* root = nullptr;
+
+    const int d[4][2] = {{-1, 0}, {0,1}, {1, 0}, {0, -1}};
+    int R, C, maxlen = 0;
 
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
 
-        Trie trie;
+        root = new Node;
         for(const string& word: words){
-            trie.insert(word);
+            insert(word);
             maxlen = max(maxlen, (int)word.size());
         }
 
-        unordered_set<string> res;
+        set<string> res;
 
-        m = board.size();
-        assert( m > 0 );
-        n = board[0].size();
-        for(int i = 0 ; i < m ; i ++)
-            for(int j = 0 ; j < n ; j ++){
-                vector<vector<bool>> visited(m, vector<bool>(n, false));
-                searchWord(board, trie, i, j, "", visited, res);
+        R = board.size();
+        assert(R > 0);
+        C = board[0].size();
+        for(int i = 0 ; i < R ; i ++)
+            for(int j = 0 ; j < C ; j ++){
+                vector<vector<bool>> visited(R, vector<bool>(C, false));
+                Node* cur = root;
+                string s = "";
+                searchWord(board, i, j, cur, s, visited, res);
             }
 
         return vector<string>(res.begin(), res.end());
@@ -106,32 +55,58 @@ public:
 
 private:
     // start from board[x][y], find word s
-    void searchWord(const vector<vector<char>> &board, Trie& trie,
-                    int x, int y, string s,
-                    vector<vector<bool>>& visited, unordered_set<string>& res){
+    void searchWord(const vector<vector<char>> &board, int x, int y, Node* cur, string& s,
+                    vector<vector<bool>>& visited, set<string>& res){
+
+        if(cur->next[board[x][y] - 'a'] == nullptr) return;
 
         s += board[x][y];
         if(s.size() > maxlen) return;
-        if(!trie.startsWith(s)) return;
-        if(trie.search(s)) res.insert(s);
+
+        cur = cur->next[board[x][y] - 'a'];
+        if(cur->end) res.insert(s);
 
         visited[x][y] = true;
         for(int i = 0 ; i < 4 ; i ++){
             int nextx = x + d[i][0];
             int nexty = y + d[i][1];
-            if(inArea(nextx, nexty) && !visited[nextx][nexty])
-                searchWord(board, trie, nextx, nexty, s, visited, res);
+            if(in_area(nextx, nexty) && !visited[nextx][nexty])
+                searchWord(board, nextx, nexty, cur, s, visited, res);
         }
         visited[x][y] = false;
+        s.pop_back();
     }
 
-    bool inArea(int x , int y){
-        return x >= 0 && x < m && y >= 0 && y < n;
+    void insert(const string& word){
+
+        Node* cur = root;
+        for(char c: word){
+            if(cur->next[c - 'a'] == nullptr)
+                cur->next[c - 'a'] = new Node();
+            cur = cur->next[c - 'a'];
+        }
+        cur->end = true;
+    }
+
+    bool in_area(int x , int y){
+        return x >= 0 && x < R && y >= 0 && y < C;
     }
 };
 
 
+void print_vec(const vector<string>& v){
+    for(const string& s: v) cout << s << ' ';
+    cout << '\n';
+}
+
 int main() {
+
+    vector<vector<char>> board1 = {{'o','a','a','n'},
+                                   {'e','t','a','e'},
+                                   {'i','h','k','r'},
+                                   {'i','f','l','v'}};
+    vector<string> words1 = {"oath","pea","eat","rain"};
+    print_vec(Solution().findWords(board1, words1));
 
     return 0;
 }
