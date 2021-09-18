03a5e5f34c5b25e826329193d599b17d64772d6a
Yubo Liu <liuyubobobo@gmail.com>
2021-09-18 12:18:35 -0700

    292 solved.

---
+++ 0001-0500/0292-Nim-Game/cpp-0292/main.cpp
@@ -0,0 +1,24 @@
+/// Source : https://leetcode.com/problems/nim-game/
+/// Author : liuyubobobo
+/// Time   : 2021-09-17
+
+#include <iostream>
+
+using namespace std;
+
+
+/// Basic Nim
+/// Time Complexity: O(1)
+/// Space Complexity: O(1)
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4;
+    }
+};
+
+
+int main() {
+
+    return 0;
+}
