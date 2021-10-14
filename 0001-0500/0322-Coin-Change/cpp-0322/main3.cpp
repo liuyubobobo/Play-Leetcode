62e8c2f80c23a86c9b7d66bf9c08154d921d8603
Yubo Liu <liuyubobobo@gmail.com>
2021-10-14 00:37:46 -0700

    0332 updated.

--- 0001-0500/0322-Coin-Change/cpp-0322/main3.cpp
+++ 0001-0500/0322-Coin-Change/cpp-0322/main3.cpp
@@ -32,36 +32,24 @@ public:
 int main() {
 
     vector<int> coins1 = {1, 2, 5};
-    int amount1 = 11;
-    cout<< Solution().coinChange(coins1, amount1) << endl;
+    cout<< Solution().coinChange(coins1, 11) << endl;
     // 3
 
-    // ---
-
     vector<int> coins2 = {2};
-    int amount2 = 1;
-    cout << Solution().coinChange(coins2, amount2) << endl;
+    cout << Solution().coinChange(coins2, 1) << endl;
     // -1
-
-    // ---
 
     vector<int> coins3 = {2};
     int amount3 = 3;
-    cout << Solution().coinChange(coins3, amount3) << endl;
+    cout << Solution().coinChange(coins3, 3) << endl;
     // -1
 
-    // ---
-
     vector<int> coins4 = {2, 5, 10, 1};
-    int amount4 = 27;
-    cout << Solution().coinChange(coins4, amount4) << endl;
+    cout << Solution().coinChange(coins4, 27) << endl;
     // 4
 
-    // ---
-
     vector<int> coins5 = {186, 419, 83, 408};
-    int amount5 = 6249;
-    cout << Solution().coinChange(coins5, amount5) << endl;
+    cout << Solution().coinChange(coins5, 6249) << endl;
     // 20
 
     return 0;
