dab0cc7a9f431bf5ba9a58774918a14d7c94ab65
Yubo Liu <liuyubobobo@gmail.com>
2021-11-22 09:36:00 -0800

    0859 bug fixed.

--- 0501-1000/0859-Buddy-Strings/cpp-0859/main.cpp
+++ 0501-1000/0859-Buddy-Strings/cpp-0859/main.cpp
@@ -1,15 +1,17 @@
 /// Source : https://leetcode.com/problems/buddy-strings/description/
 /// Author : liuyubobobo
-/// Time   : 2018-06-23
+/// Time   : 2018-06-25
+/// Updated: 2021-11-22
 
 #include <iostream>
-#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
+
 /// Scan and Compare
-/// Time Complexity: O(nlogn)
-/// Space Complexity: O(n)
+/// Time Complexity: O(n)
+/// Space Complexity: O(1)
 class Solution {
 public:
     bool buddyStrings(string A, string B) {
@@ -17,30 +19,26 @@ public:
         if(A.size() != B.size())
             return false;
 
-        unordered_map<char, char> diff;
-        int diff_num = 0;
-        for(int i = 0 ; i < A.size() ; i ++)
-            if(A[i] != B[i]){
-                diff[A[i]] = B[i];
-                diff_num ++;
-            }
-
-        if(diff_num == 0)
+        if(A == B)
             return atLeastTwoSame(A);
 
-        if(diff_num != 2)
-            return false;
+        int first = -1, second = -1;
+        for(int i = 0 ; i < A.size(); i++)
+            if(A[i] != B[i]){
+                if(first == -1)
+                    first = i;
+                else if(second == -1)
+                    second = i;
+                else
+                    return false;
+            }
 
-        unordered_map<char, char>::iterator iter1 = diff.begin();
-        unordered_map<char, char>::iterator iter2 = diff.begin();
-        iter2 ++;
-        return iter1->first == iter2->second && iter1->second == iter2->first;
+        return second != -1 && A[first] == B[second] && A[second] == B[first];
     }
 
 private:
     bool atLeastTwoSame(const string& s){
-        int freq[26];
-        memset(freq, 0, sizeof(freq));
+        vector<int> freq(26, 0);
         for(char c: s){
             freq[c - 'a'] ++;
             if(freq[c - 'a'] >= 2)
@@ -51,17 +49,22 @@ private:
 };
 
 
-void print_bool(bool res){
-    cout << (res ? "True" : "False") << endl;
-}
-
 int main() {
 
-    print_bool(Solution().buddyStrings("ab", "ba")); // true
-    print_bool(Solution().buddyStrings("ab", "ab")); // false
-    print_bool(Solution().buddyStrings("ab", "ba")); // true
-    print_bool(Solution().buddyStrings("aaaaaaabc", "aaaaaaacb")); // true
-    print_bool(Solution().buddyStrings("", "aa")); // false
+    cout << Solution().buddyStrings("ab", "ba") << endl;
+    // 1
+
+    cout << Solution().buddyStrings("ab", "ab") << endl;
+    // 0
+
+    cout << Solution().buddyStrings("ab", "ba") << endl;
+    // 1
+
+    cout << Solution().buddyStrings("aaaaaaabc", "aaaaaaacb") << endl;
+    // 1
+
+    cout << Solution().buddyStrings("", "aa") << endl;
+    // 0
 
     return 0;
 }
