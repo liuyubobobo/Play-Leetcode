5691a1b6ef0721a6d22c8d4cc1764b8238f85074
Yubo Liu <liuyubobobo@gmail.com>
2021-12-11 22:16:02 -0800

    2098 solved.

--- 2001-2500/2098-Subsequence-of-Size-K-With-the-Largest-Even-Sum/cpp-2098/main.cpp
+++ 2001-2500/2098-Subsequence-of-Size-K-With-the-Largest-Even-Sum/cpp-2098/main.cpp
@@ -1,19 +1,59 @@
+/// Source : https://leetcode.com/problems/subsequence-of-size-k-with-the-largest-even-sum/
+/// Author : liuyubobobo
+/// Time   : 2021-12-11
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 
+/// Sorting
+/// Time Complexity: O(nlogn)
+/// Space Complexity: O(n)
 class Solution {
 public:
     long long largestEvenSum(vector<int>& nums, int k) {
 
+        vector<int> odd, even;
+        for(int e: nums)
+            if(e % 2) odd.push_back(e);
+            else even.push_back(e);
 
+        sort(odd.begin(), odd.end(), greater<int>());
+        sort(even.begin(), even.end(), greater<int>());
+
+        vector<long long> odd_presum(odd.size() + 1, 0), even_presum(even.size() + 1, 0);
+        for(int i = 0; i < odd.size(); i ++)
+            odd_presum[i + 1] = odd_presum[i] + odd[i];
+        for(int i = 0; i < even.size(); i ++)
+            even_presum[i + 1] = even_presum[i] + even[i];
+
+        long long res = -1;
+        for(int odd_num = 0; odd_num <= odd.size(); odd_num += 2){
+            int even_num = k - odd_num;
+            if(even_num < 0 || even_num > even.size()) continue;
+
+            res = max(res, odd_presum[odd_num] + even_presum[even_num]);
+        }
+        return res;
     }
 };
 
 
 int main() {
+
+    vector<int> nums1 = {4, 1, 5, 3, 1};
+    cout << Solution().largestEvenSum(nums1, 3) << endl;
+    // 12
+
+    vector<int> nums2 = {4, 6, 2};
+    cout << Solution().largestEvenSum(nums2, 3) << endl;
+    // 12
+
+    vector<int> nums3 = {1, 3, 5};
+    cout << Solution().largestEvenSum(nums3, 1) << endl;
+    // -1
 
     return 0;
 }
--- readme.md
+++ readme.md
@@ -1874,6 +1874,7 @@ email: [liuyubobobo@gmail.com](mailto:liuyubobobo@gmail.com)
 | 2095 | [Delete the Middle Node of a Linked List](https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/) | [无] | [C++](2001-2500/2095-Delete-the-Middle-Node-of-a-Linked-List/cpp-2095/) | | |
 | 2096 | [Step-By-Step Directions From a Binary Tree Node to Another](https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/) | [无] | [C++](2001-2500/2096-Step-By-Step-Directions-From-a-Binary-Tree-Node-to-Another/cpp-2096/) | | |
 | 2097 | [Valid Arrangement of Pairs](https://leetcode.com/problems/valid-arrangement-of-pairs/) | [无] | [C++](2001-2500/2097-Valid-Arrangement-of-Pairs/cpp-2097/) | | |
+| 2098 | [Subsequence of Size K With the Largest Even Sum](https://leetcode.com/problems/subsequence-of-size-k-with-the-largest-even-sum/) | [无] | [C++](2001-2500/2098-Subsequence-of-Size-K-With-the-Largest-Even-Sum/cpp-2098/) | | |
 | | | | | | |
 | 2103 | [Rings and Rods](https://leetcode.com/problems/rings-and-rods/) | [无] | [C++](2001-2500/2103-Rings-and-Rods/cpp-2103/) | | |
 | 2104 | [Sum of Subarray Ranges](https://leetcode.com/problems/sum-of-subarray-ranges/) | [无] | [C++](2001-2500/2104-Sum-of-Subarray-Ranges/cpp-2104/) | | |
