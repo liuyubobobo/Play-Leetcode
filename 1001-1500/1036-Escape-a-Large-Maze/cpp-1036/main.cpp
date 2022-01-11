e19536f0ad29f63346c3ad28c446a28546efd093
Yubo Liu <liuyubobobo@gmail.com>
2022-01-10 17:03:48 -0800

    1036 codes bug fixed.

--- 1001-1500/1036-Escape-a-Large-Maze/cpp-1036/main.cpp
+++ 1001-1500/1036-Escape-a-Large-Maze/cpp-1036/main.cpp
@@ -1,57 +1,83 @@
 /// Source : https://leetcode.com/problems/escape-a-large-maze/
 /// Author : liuyubobobo
-/// Time   : 2019-04-27
+/// Time   : 2022-01-10
+
 #include <iostream>
 #include <vector>
 #include <set>
+#include <queue>
+#include <map>
 
 using namespace std;
 
-/// DFS
-/// Time Complexity: O(max_blocks^2)
-/// Space Complexity: O(max_blocks^2)
+
+/// BFS
+/// Time Complexity: O(|blocks|^2)
+/// Space Complexity: O(|blocks|^2)
 class Solution {
 
 private:
-    const int d[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    const int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int N = 1000000;
 
 public:
     bool isEscapePossible(vector<vector<int>>& blocked, vector<int>& source, vector<int>& target) {
 
         set<pair<int, int>> blockset;
         for(const vector<int>& e: blocked)
-            blockset.insert(make_pair(e[0], e[1]));
+            blockset.insert({e[0], e[1]});
 
-        set<pair<int, int>> visited;
-        if(dfs(source[0], source[1], source[0], source[1], blockset, visited))
-            return true;
-        return visited.count(make_pair(target[0], target[1]));
+        return bfs(source[0], source[1], target[0], target[1], blockset) &&
+               bfs(target[0], target[1], source[0], source[1], blockset);
     }
 
 private:
-    bool dfs(int x, int y, int sx, int sy,
-             set<pair<int, int>>& blockset, set<pair<int, int>>& visited){
+    bool bfs(int sx, int sy, int tx, int ty,
+             set<pair<int, int>>& blockset){
 
-        visited.insert(make_pair(x, y));
-        if(dis(x, y, sx, sy) > 200) return true;
-        for(int i = 0; i < 4; i ++){
-            int nextx = x + d[i][0], nexty = y + d[i][1];
-            pair<int, int> next = make_pair(nextx, nexty);
-            if(nextx >= 0 && nextx < 1000000 && nexty >= 0 && nexty < 1000000 &&
-               !blockset.count(next) && !visited.count(next))
-                if(dfs(nextx, nexty, sx, sy, blockset, visited))
-                    return true;
+        int L = blockset.size();
+
+        queue<pair<int, int>> q;
+        map<pair<int, int>, int> dis;
+        q.push({sx, sy});
+        dis[{sx, sy}] = 0;
+
+        int maxv = 0;
+        while(!q.empty()){
+            int cx = q.front().first, cy = q.front().second, v = dis[{cx, cy}];
+            q.pop();
+
+            if(cx == tx && cy == ty) return true;
+
+            maxv = max(maxv, v);
+            if(v == L) continue;
+
+            for(int d = 0; d < 4; d ++){
+                int nx = cx + dirs[d][0], ny = cy + dirs[d][1];
+                if(in_area(nx, ny) && !blockset.count({nx, ny}) && !dis.count({nx, ny})){
+                    q.push({nx, ny});
+                    dis[{nx, ny}] = v + 1;
+                }
+            }
         }
-        return false;
+        return maxv >= L;
     }
 
-    int dis(int x0, int y0, int x1, int y1){
-        return abs(x0 - x1) + abs(y0 - y1);
+    bool in_area(int x, int y){
+        return x >= 0 && x < N && y >= 0 && y < N;
     }
 };
 
 
 int main() {
+
+    vector<vector<int>> block1 = {
+            {691938,300406},{710196,624190},{858790,609485},{268029,225806},
+            {200010,188664},{132599,612099},{329444,633495},{196657,757958},{628509,883388}
+    };
+    vector<int> source1 = {655988,180910}, target1 = {267728,840949};
+    cout << Solution().isEscapePossible(block1, source1, target1) << endl;
+    // 1
 
     return 0;
 }
