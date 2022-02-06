0e1d603f5c4b37525dd4308c2d7c9ea6f74af147
Yubo Liu <liuyubobobo@gmail.com>
2022-02-05 21:09:09 -0800

    1248 comments added for my students.

--- 1001-1500/1248-Count-Number-of-Nice-Subarrays/cpp-1248/main.cpp
+++ 1001-1500/1248-Count-Number-of-Nice-Subarrays/cpp-1248/main.cpp
@@ -16,14 +16,26 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
 
+        // table[i] 存储的是当前看到有多少个位置，满足从 0 到这个位置，包含的奇数个数为 i
+        // table[3] == 5，表示当前已经看到有 5 个位置，从 0 到这个位置包含有 3 个奇数
         unordered_map<int, int> table; // odd_num -> freq
+
+        // 初始的空区间，包含有 0 个奇数
         table[0] = 1;
 
+        // cur 记录 [0, i] 区间一共有多少个奇数
         int cur = 0, res = 0;
-        for(int e: nums){
-            cur += e % 2;
+        for(int i = 0; i < nums.size(); i ++){
+            cur += nums[i] % 2; // 如果 nums[i] 是奇数，cur ++
+
+            // 查找 cur 之前有多少个位置，其奇数个数位 cur - k。
+            // 那么从 这些位置 + 1，到 cur，就一共有 k 个奇数
+            // 有 k 个奇数的区间数量 += table[cur - k]
             if(table.count(cur - k))
                 res += table[cur - k];
+
+            // 把 i 这个位置计算到 table 中
+            // 因为 [0, i] 中有 cur 个奇数，所以 table[cur] ++
             table[cur] ++;
         }
         return res;
