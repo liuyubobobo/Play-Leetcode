9ccc013e3414e9be64f57b16c8614266ffd43db5
Yubo Liu <liuyubobobo@gmail.com>
2022-04-11 19:56:13 -0700

    0289 codes updated.

--- 0001-0500/0289-Game-of-Life/cpp-0289/main.cpp
+++ 0001-0500/0289-Game-of-Life/cpp-0289/main.cpp
@@ -1,6 +1,7 @@
 /// Source : https://leetcode.com/problems/game-of-life/
 /// Author : liuyubobobo
 /// Time   : 2020-12-30
+/// Updated: 2022-04-11
 
 #include <iostream>
 #include <vector>
@@ -13,21 +14,18 @@ using namespace std;
 /// Space Complexity: O(m * n)
 class Solution {
 
-private:
-    int R, C;
-
 public:
     void gameOfLife(vector<vector<int>>& board) {
 
-        R = board.size(), C = board[0].size();
+        int R = board.size(), C = board[0].size();
         vector<vector<int>> res(R, vector<int>(C, 0));
 
         for(int i = 0; i < R; i ++)
             for(int j = 0; j < C; j ++){
 
                 int num = 0;
-                for(int ii = max(0, i - 1); ii <= min(i + 1, R + 1); ii ++)
-                    for(int jj = max(0, j - 1); jj <= min(j + 1, C + 1); jj ++){
+                for(int ii = max(0, i - 1); ii <= min(i + 1, R - 1); ii ++)
+                    for(int jj = max(0, j - 1); jj <= min(j + 1, C - 1); jj ++){
                         if(ii == i && jj == j) continue;
                         num += board[ii][jj];
                     }
