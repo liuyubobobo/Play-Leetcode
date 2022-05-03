2ab732c800e50622b76052b229b905cabc894e60
Yubo Liu <liuyubobobo@gmail.com>
2022-05-03 00:50:47 -0700

    0591 codes updated.

--- 0501-1000/0591-Tag-Validator/cpp-0591/main.cpp
+++ 0501-1000/0591-Tag-Validator/cpp-0591/main.cpp
@@ -1,24 +1,19 @@
+/// Source : https://leetcode.com/problems/tag-validator/
+/// Author : liuyubobobo
+/// Time   : 2022-05-03
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 
+/// Using Stack
+/// Time Complexity: O(n)
+/// Space Complexity: O(n)
 class Solution {
 public:
     bool isValid(string code) {
-
-        while(true){
-            int cdata_start = code.find("<![CDATA[");
-            if(cdata_start == string::npos) break;
-            if(cdata_start == 0) return false;
-
-            int cdata_end = code.find("]]>", cdata_start);
-            if(cdata_end == string::npos || cdata_end + 3 == code.size()) return false;
-
-            code = code.substr(0, cdata_start) + code.substr(cdata_end + 3);
-        }
-
 
         vector<string> tag_stack;
         int i = 0;
@@ -29,25 +24,28 @@ public:
             if(tag_start == -1) return false;
             if(i == 0 && tag_start != 0) return false;
 
-            bool is_start_tag = true;
-            string tag_name = "";
+            // end tag
             if(code[tag_start + 1] == '/'){
-                is_start_tag = false;
-                tag_name = code.substr(tag_start + 2, tag_end - (tag_start + 2));
-            }
-            else{
-                tag_name = code.substr(tag_start + 1, tag_end - (tag_start + 1));
-            }
+                string tag_name = code.substr(tag_start + 2, tag_end - (tag_start + 2));
+                if(!is_valid_tag(tag_name)) return false;
 
-            if(!is_valid_tag(tag_name)) return false;
-
-            if(is_start_tag){
-                tag_stack.push_back(tag_name);
-                i = tag_end + 1;
-            }
-            else{
                 if(tag_stack.empty() || tag_stack.back() != tag_name) return false;
                 tag_stack.pop_back();
+                i = tag_end + 1;
+            }
+            else if(code.find("<![CDATA[", tag_start) == tag_start){ // cdata tag
+                if(tag_start == 0) return false;
+
+                int cdata_end = code.find("]]>", tag_start);
+                if(cdata_end == string::npos || cdata_end + 3 == code.size()) return false;
+
+                i = cdata_end + 3;
+            }
+            else{ // start tag
+                string tag_name = code.substr(tag_start + 1, tag_end - (tag_start + 1));
+                if(!is_valid_tag(tag_name)) return false;
+
+                tag_stack.push_back(tag_name);
                 i = tag_end + 1;
             }
 
@@ -99,6 +97,9 @@ int main() {
     // 0
 
     cout << Solution().isValid("<TAG>sometext</TAG><![CDATA[ABC]]>") << '\n';
+    // 0
+
+    cout << Solution().isValid("<TAG>sometext</TA<![CDATA[ABC]]>G>") << '\n';
     // 0
 
     return 0;
