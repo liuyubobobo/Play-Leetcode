a0f02459398d88ff507774dd6a00049c24ab4965
Yubo Liu <liuyubobobo@gmail.com>
2022-05-24 14:15:50 -0700

    0456 new algo updated.

--- 0001-0500/0456-132-Pattern/cpp-0456/main.cpp
+++ 0001-0500/0456-132-Pattern/cpp-0456/main.cpp
@@ -1,40 +1,37 @@
 /// Source : https://leetcode.com/problems/132-pattern/
 /// Author : liuyubobobo
 /// Time   : 2021-03-23
+/// Updated: 2022-05-24
 
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <set>
 
 using namespace std;
 
 
-/// Brute Force to check the middle
-/// Time Complexity: O(n^2)
-/// Space Complexity: O(1)
+/// Reverse to find 32-pattern first and greedily check 1-part
+/// Time Complexity: O(nlogn)
+/// Space Complexity: O(n)
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
 
         int n = nums.size();
-        for(int i = 0; i < n; i ++){
+        if(n <= 2) return false;
 
-            int leftmin = INT_MAX;
-            for(int j = 0; j < i; j ++)
-                if(nums[j] < nums[i] && nums[j] < leftmin)
-                    leftmin = nums[j];
+        set<int, greater<int>> s;
+        s.insert(nums[n - 1]);
+        s.insert(nums[n - 2]);
+        int t = nums[n - 2] > nums[n - 1] ? nums[n - 1] : INT_MIN;
 
-            if(leftmin == INT_MAX) continue;
+        for(int i = n - 3; i >= 0; i --){
+            if(nums[i] < t) return true;
 
-            int rightmax = INT_MIN;
-            for(int j = i + 1; j < n; j ++)
-                if(nums[j] < nums[i] && nums[j] > rightmax)
-                    rightmax = nums[j];
-
-            if(rightmax == INT_MIN) continue;
-
-            if(leftmin < rightmax)
-                return true;
+            auto iter = s.upper_bound(nums[i]);
+            if(iter != s.end())
+                t = max(t, *iter);
+            s.insert(nums[i]);
         }
 
         return false;
@@ -44,9 +41,17 @@ public:
 
 int main() {
 
-    vector<int> nums1 = {1, 0, 1, -4, -3};
+    vector<int> nums1 = {1, 2, 3, 4};
     cout << Solution().find132pattern(nums1) << endl;
     // 0
+
+    vector<int> nums2 = {1, 0, 1, -4, -3};
+    cout << Solution().find132pattern(nums2) << endl;
+    // 0
+
+    vector<int> nums3 = {1, 3, 2, 4, 5};
+    cout << Solution().find132pattern(nums3) << endl;
+    // 1
 
     return 0;
 }
