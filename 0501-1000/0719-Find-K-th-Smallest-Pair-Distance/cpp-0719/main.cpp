cd6632da164222f51fa34794fe40876234302776
Yubo Liu <liuyubobobo@gmail.com>
2022-06-14 16:08:39 -0700

    0719 updated.

--- 0501-1000/0719-Find-K-th-Smallest-Pair-Distance/cpp-0719/main.cpp
+++ 0501-1000/0719-Find-K-th-Smallest-Pair-Distance/cpp-0719/main.cpp
@@ -1,56 +1,58 @@
 /// Source : https://leetcode.com/problems/find-k-th-smallest-pair-distance/description/
 /// Author : liuyubobobo
-/// Time   : 2017-10-28
+/// Time   : 2022-06-14
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
 
 using namespace std;
 
-/// Time Complexity: O(nlogn + n^2 + W)
-/// Space Complexity: O(W), where W = max(nums) - min(nums)
+
+/// Binary Search
+/// Time Complexity: O(log(max_dis) * nlogn)
+/// Space Complexity: O(1)
 class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
 
-        int dis[1000000];
-        for(int i = 0 ; i < 1000000 ; i ++)
-            dis[i] = 0;
+        int n = nums.size();
 
         sort(nums.begin(), nums.end());
-        for(int i = 0 ; i < nums.size() ; i ++)
-            for(int j = i + 1 ; j < nums.size() ; j ++){
-                //cout << nums[j] - nums[i] << endl;
-                dis[nums[j]-nums[i]] ++;
-            }
-
-        int index = 0;
-        for(int i = 0 ; i < 1000000 ; i ++){
-            index += dis[i];
-            if(k <= index)
-                return i;
+        int l = 0, r = nums.back() - nums[0];
+        while(l < r){
+            int d = (l + r) / 2;
+            if(dis_cnt_less_than_or_equal_to(n, nums, d) >= k) r = d;
+            else l = d + 1;
         }
+        return l;
+    }
 
-        return -1;
+private:
+    int dis_cnt_less_than_or_equal_to(int n, const vector<int>& nums, int d){
+
+        int cnt = 0;
+        for(int i = 0; i < n; i ++){
+            auto iter = upper_bound(nums.begin(), nums.end(), nums[i] + d);
+            cnt += iter - nums.begin() - i - 1;
+        }
+        return cnt;
     }
 };
 
+
 int main() {
 
-    int nums1[] = {1, 3, 1};
-    int k1 = 1;
-    vector<int> vec1(nums1, nums1 + sizeof(nums1)/sizeof(int));
-    cout << Solution().smallestDistancePair(vec1, k1) << endl;
+    vector<int> nums1 = {1, 3, 1};
+    cout << Solution().smallestDistancePair(nums1, 1) << endl;
     // 0
 
-    // ---
+    vector<int> nums2 = {1, 1, 1};
+    cout << Solution().smallestDistancePair(nums2, 2) << endl;
+    // 0
 
-    int nums2[] = {1, 6, 1};
-    int k2 = 3;
-    vector<int> vec2(nums2, nums2 + sizeof(nums2)/sizeof(int));
-    cout << Solution().smallestDistancePair(vec2, k2) << endl;
+    vector<int> nums3 = {1, 6, 1};
+    cout << Solution().smallestDistancePair(nums3, 3) << endl;
     // 5
 
     return 0;
