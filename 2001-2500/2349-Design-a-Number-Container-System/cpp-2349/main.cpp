fccb867f141f0acb4644905b85d769010bdf72ab
Yubo Liu <liuyubobobo@gmail.com>
2022-07-23 20:52:16 -0700

    2349 solved.

---
+++ 2001-2500/2349-Design-a-Number-Container-System/cpp-2349/main.cpp
@@ -0,0 +1,48 @@
+/// Source : https://leetcode.com/problems/design-a-number-container-system/
+/// Author : liuyubobobo
+/// Time   : 2022-07-23
+
+#include <iostream>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+/// Using Map
+class NumberContainers {
+
+private:
+    map<int, int> data;
+    map<int, set<int>> num2indice;
+
+public:
+    NumberContainers() {}
+
+    void change(int index, int number) {
+
+        auto iter = data.find(index);
+        if(iter == data.end()){
+            data[index] = number;
+            num2indice[number].insert(index);
+            return;
+        }
+
+        int old_number = iter->second;
+        num2indice[old_number].erase(index);
+        if(num2indice[old_number].empty()) num2indice.erase(old_number);
+
+        data[index] = number;
+        num2indice[number].insert(index);
+    }
+
+    int find(int number) {
+        return num2indice[number].empty() ? -1 : *num2indice[number].begin();
+    }
+};
+
+
+int main() {
+
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -2199,6 +2199,7 @@ email: [liuyubobobo@gmail.com](mailto:liuyubobobo@gmail.com)
 | 2346 | Database Problem: [Link](https://github.com/liuyubobobo/Play-Leetcode-Database/) | - | - | - | - |
 | 2347 | [Best Poker Hand](https://leetcode.com/problems/best-poker-hand/) | [无] | [C++](2001-2500/2347-Best-Poker-Hand/cpp-2347/) | | |
 | 2348 | [Number of Zero-Filled Subarrays](https://leetcode.com/problems/number-of-zero-filled-subarrays/) | [无] | [C++](2001-2500/2348-Number-of-Zero-Filled-Subarrays/cpp-2348/) | | |
+| 2349 | [Design a Number Container System](https://leetcode.com/problems/design-a-number-container-system/) | [无] | [C++](2001-2500/2349-Design-a-Number-Container-System/cpp-2349/) | | |
 | | | | | | |
 
 ## 力扣中文站比赛
