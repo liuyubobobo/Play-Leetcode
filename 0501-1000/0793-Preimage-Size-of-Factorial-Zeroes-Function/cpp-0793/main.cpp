1ba1ac95c0971a0a6689b0c3459d7830ea4df037
Yubo Liu <liuyubobobo@gmail.com>
2022-08-28 00:29:32 -0700

    0793 bug fixed.

--- 0501-1000/0793-Preimage-Size-of-Factorial-Zeroes-Function/cpp-0793/main.cpp
+++ 0501-1000/0793-Preimage-Size-of-Factorial-Zeroes-Function/cpp-0793/main.cpp
@@ -1,10 +1,12 @@
 /// Source : https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/description/
 /// Author : liuyubobobo
 /// Time   : 2018-03-03
+/// Updated: 2022-08-28
 
 #include <iostream>
 
 using namespace std;
+
 
 /// Binary Search
 /// Time Complexity: O(logK * logK)
@@ -16,25 +18,26 @@ public:
         if(K == 0)
             return 5;
 
-        int l = 0, r = 1000000000;
-        while(l <= r){
-            int mid = (l + r) / 2;
-            int f_res = f(mid);
-            if(f_res == K)
-                return 5;
-            else if(f_res > K)
-                r = mid - 1;
+        long long l = 0ll, r = 5ll * K;
+        while(l < r){
+            long long mid = (l + r) / 2;
+            long long f_res = f(mid);
+            if(f_res >= K)
+                r = mid;
             else
                 l = mid + 1;
         }
 
+        if(f(l) == K){
+            return 5 - l % 5;
+        }
         return 0;
     }
 
 private:
-    int f(int x){
-        int res = 0;
-        int factor = 5;
+    long long f(long long x){
+        long long res = 0;
+        long long factor = 5;
         while(factor <= x){
             res += x / factor;
             factor *= 5;
@@ -43,10 +46,17 @@ private:
     }
 };
 
+
 int main() {
 
     cout << Solution().preimageSizeFZF(0) << endl;
+    // 5
+
     cout << Solution().preimageSizeFZF(5) << endl;
+    // 0
+
+    cout << Solution().preimageSizeFZF(1000000000) << endl;
+    // 5
 
     return 0;
 }
