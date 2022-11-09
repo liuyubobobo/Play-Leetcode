4075a073b14535ba22737751406837068afe0f4a
Yubo Liu <liuyubobobo@gmail.com>
2022-11-09 11:08:47 -0800

    0864 codes updated.

--- 0501-1000/0864-Shortest-Path-to-Get-All-Keys/cpp-0864/main.cpp
+++ 0501-1000/0864-Shortest-Path-to-Get-All-Keys/cpp-0864/main.cpp
@@ -1,135 +1,74 @@
 /// Source : https://leetcode.com/problems/shortest-path-to-get-all-keys/description/
 /// Author : liuyubobobo
 /// Time   : 2018-07-09
+/// Updated: 2022-11-09
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <unordered_map>
-#include <unordered_set>
 #include <cassert>
 #include <queue>
 
 using namespace std;
 
-/// Brute Force all Keys Permutation
-/// Time Complexity: O(keys!*keys*n^2)
-/// Space Complexity:O(keys + n^2)
+/// BFS
+/// Time Complexity: O(R * C * (2 ^ key_cnt))
+/// Space Complexity:O(R * C * (2 ^ key_cnt))
 class Solution {
 
 private:
-    int n, m;
-    const int d[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    const int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
 public:
     int shortestPathAllKeys(vector<string>& grid) {
 
-        n = grid.size();
-        m = grid[0].size();
+        int R = grid.size(), C = grid[0].size();
 
-        unordered_map<char, pair<int, int>> poi = getPOI(grid);
+        int sx = -1, sy = -1, key_cnt = 0;
+        for(int i = 0; i < R; i ++)
+            for(int j = 0; j < C; j ++){
+                if(grid[i][j] == '@'){
+                    sx = i, sy = j;
+                }
+                else if(islower(grid[i][j])){
+                    key_cnt ++;
+                }
+            }
+        assert(sx != -1 && sy != -1);
 
-        vector<char> keys = getAllKeys(grid);
-        sort(keys.begin(), keys.end());
-
-        int res = INT_MAX;
-        do{
-            res = min(res, go(grid, keys, poi, res));
-        }while(next_permutation(keys.begin(), keys.end()));
-
-        if(res == INT_MAX)
-            return -1;
-        return res;
-    }
-
-private:
-    int go(const vector<string>& grid, const vector<char>& keys,
-           unordered_map<char, pair<int, int>>& poi, int curMinRes){
-
-        vector<pair<int, int>> pos = {poi['@']};
-        for(char key: keys)
-            pos.push_back(poi[key]);
-
-        int res = 0;
-        unordered_set<char> ownKeys;
-        for(int i = 1; i < pos.size() ; i ++){
-            int need = go(grid, pos[i-1], pos[i], ownKeys);
-            if(need == -1)
-                return INT_MAX;
-
-            res += need;
-            if(res >= curMinRes)
-                return INT_MAX;
-            ownKeys.insert(keys[i-1]);
-        }
-        return res;
-    }
-
-    int go(const vector<string>& grid,
-           const pair<int, int>& start, const pair<int, int>& end,
-           const unordered_set<char>& ownKeys){
-
-        queue<pair<pair<int, int>, int>> q;
-        q.push(make_pair(start, 0));
-
-        unordered_set<int> visited;
-        visited.insert(start.first * m + start.second);
-
+        vector<vector<int>> dis(R * C, vector<int>((1 << key_cnt), -1));
+        dis[sx * C + sy][0] = 0;
+        queue<pair<int, int>> q;
+        q.push({sx * C + sy, 0});
         while(!q.empty()){
-            pair<int, int> cur = q.front().first;
-            int step = q.front().second;
+            int cpos = q.front().first, key_state = q.front().second;
+            int cx = cpos / C, cy = cpos % C;
             q.pop();
 
-            for(int i = 0 ; i < 4 ; i ++){
-                int nextX = cur.first + d[i][0];
-                int nextY = cur.second + d[i][1];
-                if(nextX == end.first && nextY == end.second)
-                    return step + 1;
-                if(inArea(nextX, nextY) && grid[nextX][nextY] != '#' &&
-                        visited.find(nextX * m + nextY) == visited.end()){
+            if(key_state == (1 << key_cnt) - 1) return dis[cpos][key_state];
 
-                    if(!isLock(grid[nextX][nextY]) ||
-                            (isLock(grid[nextX][nextY]) && ownKeys.find(grid[nextX][nextY] - 'A' + 'a') != ownKeys.end())){
-                        q.push(make_pair(make_pair(nextX, nextY), step + 1));
-                        visited.insert(nextX * m + nextY);
-                    }
-                }
+            for(int d = 0; d < 4; d ++){
+                int nx = cx + dirs[d][0], ny = cy + dirs[d][1];
+                if(!in_area(R, C, nx, ny) || grid[nx][ny] == '#') continue;
+
+                if(isupper(grid[nx][ny]) && ((1 << (grid[nx][ny] - 'A')) & key_state) == 0)
+                    continue;
+
+                int npos = nx * C + ny;
+                int next_key_state = key_state;
+                if(islower(grid[nx][ny])) next_key_state |= (1 << (grid[nx][ny] - 'a'));
+
+                if(dis[npos][next_key_state] != -1) continue;
+
+                dis[npos][next_key_state] = dis[cpos][key_state] + 1;
+                q.push({npos, next_key_state});
             }
         }
         return -1;
     }
 
-    vector<char> getAllKeys(const vector<string>& grid){
-        vector<char> res;
-        for(int i = 0 ; i < n ; i ++)
-            for(int j = 0 ; j < m ; j ++)
-                if(isKey(grid[i][j]))
-                    res.push_back(grid[i][j]);
-        return res;
-    }
-
-    unordered_map<char, pair<int, int>> getPOI(const vector<string>& grid){
-        unordered_map<char, pair<int, int>> res;
-        for(int i = 0 ; i < n ; i ++)
-            for(int j = 0 ; j < m ; j ++)
-                if(grid[i][j] != '.' && grid[i][j] != '#'){
-                    assert(grid[i][j] == '@' || isKey(grid[i][j]) || isLock(grid[i][j]));
-                    assert(res.find(grid[i][j]) == res.end());
-                    res[grid[i][j]] = make_pair(i, j);
-                }
-        return res;
-    };
-
-    bool isKey(char c){
-        return c >= 'a' && c <= 'z';
-    }
-
-    bool isLock(char c){
-        return c >= 'A' && c <= 'Z';
-    }
-
-    bool inArea(int x, int y){
-        return x >= 0 && x < n && y >= 0 && y < m;
+private:
+    bool in_area(int R, int C, int x, int y){
+        return x >= 0 && x < R && y >= 0 && y < C;
     }
 };
 
@@ -140,48 +79,17 @@ int main() {
                             "###.#",
                             "b.A.B"};
     cout << Solution().shortestPathAllKeys(grid1) << endl;
+    // 8
 
     vector<string> grid2 = {"@..aA",
                             "..B#.",
                             "....b"};
     cout << Solution().shortestPathAllKeys(grid2) << endl;
+    // 6
 
-    vector<string> grid3 = {"@abcdeABCDEFf"};
+    vector<string> grid3 = {"@Aa"};
     cout << Solution().shortestPathAllKeys(grid3) << endl;
-
-    vector<string> grid4 = {
-            "#..#.#.#..#.#.#.....#......#..",
-            ".#.......#....#A.....#.#......",
-            "#....#.....#.........#........",
-            "...#.#.........#..@....#....#.",
-            ".#.#.##...#.........##....#..#",
-            "..........#..#..###....##..#.#",
-            ".......#......#...#...#.....c#",
-            ".#...#.##......#...#.###...#..",
-            "..........##...#.......#......",
-            "#...#.........a#....#.#.##....",
-            "..#..#...#...#..#....#.....##.",
-            "..........#...#.##............",
-            "...#....#..#.........#..D.....",
-            "....#E.#....##................",
-            "...........##.#.......#.#....#",
-            "...#..#...#.#............#e...",
-            "..#####....#.#...........##..#",
-            "##......##......#.#...#..#.#..",
-            ".#F.......#..##.......#....#..",
-            "............#....#..#..#...#..",
-            ".............#...#f...#..##...",
-            "....#..#...##.........#..#..#.",
-            ".....#.....##.###..##.#......#",
-            ".#..#.#...#.....#........###..",
-            ".....#.#...#...#.....#.....#..",
-            "##.....#....B.....#..#b.......",
-            ".####....##..#.##..d.#......#.",
-            "..#.....#....##........##...##",
-            "...#...#...C..#..#....#.......",
-            "#.....##.....#.#......#......."
-    };
-    cout << Solution().shortestPathAllKeys(grid4) << endl;
+    // -1
 
     return 0;
 }
