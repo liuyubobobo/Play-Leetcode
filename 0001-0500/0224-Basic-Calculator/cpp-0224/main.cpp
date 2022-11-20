8bf0089943de41f8f002ec389c752efcc6e2bf8c
Yubo Liu <liuyubobobo@gmail.com>
2022-11-20 00:22:37 -0800

    0224 bug fixed.

--- 0001-0500/0224-Basic-Calculator/cpp-0224/main.cpp
+++ 0001-0500/0224-Basic-Calculator/cpp-0224/main.cpp
@@ -1,7 +1,7 @@
 /// Source : https://leetcode.com/problems/basic-calculator/description/
 /// Author : liuyubobobo
 /// Time   : 2018-09-03
-/// Updated: 2021-03-09
+/// Updated: 2022-11-20
 
 #include <iostream>
 #include <vector>
@@ -16,41 +16,53 @@ using namespace std;
 /// Space Complexity: O(n)
 class Solution {
 public:
-    int calculate(string s) {
+    int calculate(string s2) {
+
+        string s;
+        for(char c: s2) if(c != ' ') s += c;
+
+        int n = s.size();
+        vector<int> right(n, -1);
+        vector<int> left;
+        for(int i = 0; i < n; i ++) {
+            if (s[i] == '(') left.push_back(i);
+            else if (s[i] == ')') right[left.back()] = i, left.pop_back();
+        }
+
+        return parse(s, 0, n - 1, right);
+    }
+
+private:
+    int parse(const string& s, int l, int r, const vector<int>& right){
 
         vector<int> nums;
         vector<char> ops;
 
-        for(int i = 0; i < s.size(); ){
-            if(s[i] == '+' || s[i] == '-' || s[i] == '(')
+        for(int i = l; i <= r; ){
+            if(s[i] == '+' || s[i] == '-'){
                 ops.push_back(s[i++]);
-            else if(s[i] == ')'){
-                assert(!ops.empty() && ops.back() == '(');
-                ops.pop_back();
-                i ++;
-
-                cal(nums, ops);
+                if(nums.empty()) nums.push_back(0);
             }
-            else if(isdigit(s[i])){
-
-                int num = s[i] - '0';
-                int j;
-                for(j = i + 1; j < s.size() && isdigit(s[j]); j ++)
+            else if(s[i] == '('){
+                nums.push_back(parse(s, i + 1, right[i] - 1, right));
+                cal(nums, ops);
+                i = right[i] + 1;
+            }
+            else{
+                int j = i, num = 0;
+                for(; j <= r && isdigit(s[j]); j ++)
                     num = num * 10 + (s[j] - '0');
-                i = j;
 
                 nums.push_back(num);
                 cal(nums, ops);
+                i = j;
             }
-            else
-                i ++;
         }
 
         assert(nums.size() == 1);
         return nums.back();
     }
 
-private:
     void cal(vector<int>& nums, vector<char>& ops){
 
         if(!ops.empty() && (ops.back() == '+' || ops.back() == '-')){
@@ -74,6 +86,8 @@ int main() {
     cout << Solution().calculate(" 2-1 + 2 ") << endl; // 3
     cout << Solution().calculate("(1+(4+5+2)-3)+(6+8)") << endl; // 23
     cout << Solution().calculate("-2+ 1") << endl; // -1
+    cout << Solution().calculate("1-(-2)") << endl; // 3
+    cout << Solution().calculate("-(3+(4+5))") << endl; // -12
 
     return 0;
 }
