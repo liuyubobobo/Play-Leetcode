d788cf3a0bd35cca88e954f0e0406d81ec3b8912
Yubo Liu <liuyubobobo@gmail.com>
2023-04-21 12:14:31 -0700

    1027 codes updated.

--- 1001-1500/1027-Longest-Arithmetic-Sequence/cpp-1027/main.cpp
+++ 1001-1500/1027-Longest-Arithmetic-Sequence/cpp-1027/main.cpp
@@ -1,45 +1,37 @@
 /// Source : https://leetcode.com/problems/longest-arithmetic-sequence/
 /// Author : liuyubobobo
 /// Time   : 2019-04-13
+/// Updated: 2023-04-13
 
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 
-/// HashMap + Bianry Search
-/// Time Complexity: O(n^2*logn)
+/// DP
+/// Time Complexity: O(n^2)
 /// Space Complexity: O(n^2)
 class Solution {
+
+private:
+    const int offset = 500;
 
 public:
     int longestArithSeqLength(vector<int>& A) {
 
-        unordered_map<int, vector<int>> map;
-        for(int i = 0; i < A.size(); i ++)
-            map[A[i]].push_back(i);
+        int n = A.size();
+        vector<vector<int>> dp(n, vector<int>(1001, 1));
 
-        int res = 2;
-        for(int i = 0; i < A.size(); i ++)
-            for(int j = i + 1; j < A.size(); j ++)
-                res = max(res, length(j, A[j], A[j] - A[i], map));
-        return res;
-    }
+        int res = 1;
+        for(int i = 1; i < n; i ++)
+            for(int j = i - 1; j >= 0; j --){
+                int d = A[i] - A[j] + offset;
+                dp[i][d] = max(dp[i][d], dp[j][d] + 1);
+                res = max(res, dp[i][d]);
+            }
 
-private:
-    int length(int pos, int cur, int diff, unordered_map<int, vector<int>>& map){
-
-        int res = 2;
-        while(true){
-            cur += diff;
-            vector<int>::iterator iter = upper_bound(map[cur].begin(), map[cur].end(), pos);
-            if(iter == map[cur].end()) break;
-
-            res ++;
-            pos = *iter;
-        }
         return res;
     }
 };
