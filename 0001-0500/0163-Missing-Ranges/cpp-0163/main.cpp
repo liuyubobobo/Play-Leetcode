4184b6484efab44ab40e900a1da40a79bd8449c6
Yubo Liu <liuyubobobo@gmail.com>
2023-06-16 12:52:59 -0700

    0163 codes updated.

--- 0001-0500/0163-Missing-Ranges/cpp-0163/main.cpp
+++ 0001-0500/0163-Missing-Ranges/cpp-0163/main.cpp
@@ -1,6 +1,7 @@
 /// Source : https://leetcode.com/problems/missing-ranges/
 /// Author : liuyubobobo
 /// Time   : 2020-12-08
+/// Updated: 2022-06-16
 
 #include <iostream>
 #include <vector>
@@ -13,24 +14,33 @@ using namespace std;
 /// Space Complexity: O(1)
 class Solution {
 public:
-    vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+    vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
 
-        nums.insert(nums.begin(), lower - 1);
         nums.push_back(upper + 1);
 
-        vector<string> res;
-        for(int i = 0; i + 1 < nums.size(); i ++)
-            if(nums[i] + 1 != nums[i + 1]){
-                int l = nums[i] + 1, r = nums[i + 1] - 1;
-                if(l == r) res.push_back(to_string(l));
-                else res.push_back(to_string(l) + "->" + to_string(r));
+        vector<vector<int>> res;
+        int l = lower;
+        for(int i = 0; i < nums.size(); i ++){
+            if(nums[i] == l) l ++;
+            else{
+                res.push_back({l, nums[i] - 1});
+                l = nums[i] + 1;
             }
+        }
         return res;
     }
 };
 
 
+void print_vec(const vector<vector<int>>& v){
+    for(const vector<int>& seg: v) cout << '(' << seg[0] << ',' << seg[1] << ')';
+    cout << '\n';
+}
+
 int main() {
+
+    vector<int> nums1 = {0, 1, 3, 50, 75};
+    print_vec(Solution().findMissingRanges(nums1, 0, 99));
 
     return 0;
 }
